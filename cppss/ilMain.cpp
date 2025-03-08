4ef522f6a1a750ca450af93417680b8f94428bbf
Mauro Ghirardi <mauro.ghirardi01@universitadipavia.it>
2025-03-08 14:20:16 +0100

    main

--- cppss/ilMain.cpp
+++ cppss/ilMain.cpp
@@ -22,7 +22,7 @@ void linee(TH1I *h);
 
 int main(int argc, char** argv){
 
-	if(argc!=6){
+	if(argc!=7){
 		std::cerr << "Usage: configFile numeroAnni granularità mode outputfile\n"; 
 		std::cerr << "   configFile: file di configurazione delle condizioni iniziali\n";
 		std::cerr << "   numeroAnni: anni di evoluzione\n";
@@ -41,7 +41,7 @@ int main(int argc, char** argv){
 	uint32_t ddt = atoi(argv[3]);
 	uint32_t mode  = atoi(argv[4]);
 	uint32_t rel  = atoi(argv[5]);
-	std::string outFile = argv[5];
+	std::string outFile = argv[6];
 	int step=0;
 	
 	TApplication myApp("App", &argc, argv);
