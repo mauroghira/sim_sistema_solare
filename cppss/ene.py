76f0b45f0a5d67df41e14a81e771d12aaeaf630e
Mauro Ghirardi <mauro.ghirardi01@universitadipavia.it>
2025-03-19 16:02:20 +0100

    energia

--- cppss/ene.py
+++ cppss/ene.py
@@ -21,6 +21,98 @@ def R_squared(x, y, func, *params):
     r_squared = 1 - (ss_res/ss_tot)
     return r_squared
 
+######
+
+def fit_mobile(mec):
+    ndata = len(mec.index)
+    serie=mec.name
+    if serie=="Emec":
+        guess=[-1.958621e35, 5.8e29, 12, -np.pi, 2.5e29, 0.2405, 0, -5e26]
+        inf=[min(mec), 3e29, 10, -np.pi, 1.5e29, 0.235, -np.pi, -1e27]
+        sup=[max(mec), 10e29, 15, np.pi, 10e29, 0.25, np.pi, -1e26]
+    elif serie=="-Ecin" or serie=="Epot/2":
+        guess=[mec.mean(), 15e33, 12.03, np.pi/2, 2.5e27	, 0.2405, 0, -0]
+        inf=[min(mec), 5e33, 10, -np.pi, 1.5e27, 0.235, -np.pi, -np.inf]
+        sup=[max(mec), 5e35, 15, np.pi, 10e27, 0.25, np.pi, 0]
+    elif serie=="ddd":
+        guess=[mec.mean(), 4.2e10, 12.03, np.pi/2, 0	, 0.2405, 0, -0]
+        inf=[min(mec), 1e10, 10, -np.pi, 0, 0.235, -np.pi, -np.inf]
+        sup=[max(mec), 5e10, 15, np.pi, 1, 0.25, np.pi, np.inf]
+
+    popt, pcov = sp.optimize.curve_fit(cos_func, mec.index, mec, p0=guess, bounds=[inf,sup])
+    R2 = R_squared(mec.index, mec, cos_func, *popt)
+    return popt, pcov, R2
+
+#######
+
+def fit_fisso(mec):
+    ndata = len(mec.index)
+    serie=mec.name
+    guess=[-1.9586215e35, 0.7e28, 0.71, -np.pi, 0.7e28, 0.07, 0, -4e26]
+    inf=[min(mec), 1e27, 0.235, -np.pi, 1.5e27, 0, -np.pi, -1e27]
+    sup=[max(mec), 10e28, 1, np.pi, 10e28, 0.1, np.pi, -1e26]
+
+    popt, pcov = sp.optimize.curve_fit(cos_func, mec.index, mec, p0=guess, bounds=[inf,sup])
+    sigma = np.sqrt(ndata * np.diag(pcov))
+    R2 = R_squared(mec.index, mec, cos_func, *popt)
+    return popt, pcov, R2
+
+#########
+
+def stampa(popt, pcov, R2, ndata):
+    sigma = np.sqrt(ndata * np.diag(pcov))
+    print(f"Valore di partenza dell'oscillazione = {popt[0]} +/- {sigma[1]}")
+    print(f"Ampiezza portante = {popt[1]} +/- {sigma[1]}")
+    print(f"Periodo portante = {popt[2]} +/- {sigma[2]}")
+    print(f"fase portante = {popt[3]*180/np.pi} +/- {sigma[3]*180/np.pi}")
+    print(f"Ampiezza modulante = {popt[4]} +/- {sigma[4]}")
+    print(f"Periodo modulante = {popt[5]*365.26} +/- {sigma[5]*365.26}")
+    print(f"fase modulante= {popt[6]*180/np.pi} +/- {sigma[6]*180/np.pi}")
+    print(f"pendenza decrescita= {popt[7]} +/- {sigma[7]}")
+    print(f"R^2 = {R2}")
+
+#######
+
+def Evst(mec, popt):
+    serie=mec.name
+    ndata = len(mec.index)
+    """confronto sullo stesso grafico
+    plt.figure(figsize=(10,6))
+    plt.title("Energia nel tempo")
+    plt.xlabel("tempo [anni]")
+    plt.ylabel("Energia [J]")
+    plt.plot(mec.index, mec, label=serie)
+    plt.plot(mec.index, cos_func(mec.index, *popt), '-', label='Fit')
+    plt.legend()
+    plt.grid(True)
+    plt.show()
+    """
+    fig, axs = plt.subplots(2, figsize=(10,6))
+    fig.suptitle('Energia nel tempo')
+    axs[1].plot(mec.index, cos_func(mec.index, *popt), '-', label='Fit')
+    axs[0].plot(mec.index, mec, label=serie)
+    axs[1].set_title('f(t)=A+Bcos($2 \pi t/C$-D)+Ecos($2\pi t/F$-G)+Ht')
+    for ax in axs.flat:
+        ax.set(xlabel='Tempo [anni]', ylabel='Energia [J]')
+        ax.grid(True)
+        ax.set_xlim(0,mec.index.max())
+        ax.label_outer()
+    plt.legend()
+    plt.show()
+
+#####
+
+def delta(mec):
+    ndata = len(mec.index)
+    plt.figure(figsize=(10,6))
+    plt.title("variazione Energia meccanica nel tempo")
+    plt.xlabel("tempo [anni]")
+    plt.ylabel("$\Delta$E [J]")
+    plt.plot(mec.index, mec-mec.iloc[0])
+    plt.show()
+
+#####
+
 SMALL_SIZE = 14
 MEDIUM_SIZE = 15
 BIGGER_SIZE = 20
@@ -33,6 +125,7 @@ plt.rc('ytick', labelsize=SMALL_SIZE)    # fontsize of the tick labels
 plt.rc('legend', fontsize=SMALL_SIZE)    # legend fontsize
 plt.rc('figure', titlesize=BIGGER_SIZE)  # fontsize of the figure title
 
+#passo i file coi dati dq linea di comando delle energie e distanze dal sole di un pianeta
 df = []
 for arg in sys.argv:
 	if arg!="ene.py":
@@ -43,187 +136,36 @@ for arg in sys.argv:
 
 #df=df.drop('0', axis=1)
 ene=df[0];
-dist=df[1]
-dist.set_index('0', inplace=True)
+#dist=df[1]
+#dist.set_index('0', inplace=True)
 ene.set_index('0', inplace=True)
 ene.columns = ['Epot/2', '-Ecin', 'Emec']
-dist.columns = ['ddd']
-#print(df)
+#dist.columns = ['ddd']
+#print(ene["Emec"].name)
+ene=ene.head(int(len(ene.index)/15))
 
-"""
-#ene=ene.head(int(len(ene.index)/15))
-mec=ene['Emec']
-ndata = len(ene.index)
-guess=[-1.958621e35, 5.8e29, 12, -np.pi, 2.5e29, 0.2405, 0, -5e26]
-inf=[min(mec), 3e29, 10, -np.pi, 1.5e29, 0.235, -np.pi, -1e27]
-sup=[max(mec), 10e29, 15, np.pi, 10e29, 0.25, np.pi, -1e26]
-popt, pcov = sp.optimize.curve_fit(cos_func, ene.index, ene['Emec'], p0=guess, bounds=[inf,sup])
-sigma = np.sqrt(ndata * np.diag(pcov))
-R2 = R_squared(ene.index, mec, cos_func, *popt)
-
-print(f"Valore di partenza dell'oscillazione = {popt[0]} +/- {sigma[1]}")
-print(f"Ampiezza portante = {popt[1]} +/- {sigma[1]}")
-print(f"Periodo portante = {popt[2]} +/- {sigma[2]}")
-print(f"fase portante = {popt[3]*180/np.pi} +/- {sigma[3]*180/np.pi}")
-print(f"Ampiezza modulante = {popt[4]} +/- {sigma[4]}")
-print(f"Periodo modulante = {popt[5]*365.26} +/- {sigma[5]*365.26}")
-print(f"fase modulante= {popt[6]*180/np.pi} +/- {sigma[6]*180/np.pi}")
-print(f"pendenza decrescita= {popt[7]} +/- {sigma[7]}")
-print(f"R^2 = {R2}")
-
-plt.figure(figsize=(18,8))
-plt.title("Energia meccanica nel tempo")
-plt.xlabel("tempo [step]")
-plt.ylabel("Energia [J]")
-plt.plot(ene.index, mec, label='Emec')
-plt.plot(ene.index, cos_func(ene.index, *popt), '-', label='Fit')
-plt.legend()
-plt.show()
-
-fig, axs = plt.subplots(2, figsize=(19,10))
-fig.suptitle('Energia meccanica nel tempo')
-axs[1].plot(ene.index, cos_func(ene.index, *popt), '-', label='Fit')
-axs[0].plot(ene.index, mec, label='Emec')
-axs[1].set_title('f(t)=A+Bcos($2 \pi t/C$-D)+Ecos($2\pi t/F$-G)+Ht')
-for ax in axs.flat:
-    ax.set(xlabel='Tempo [anni]', ylabel='Energia [J]')
-for ax in axs.flat:
-    ax.label_outer()
-plt.legend()
-plt.show()
+#"""fit con sole in moto, tiene periodo giove ecc
+mec=ene["Emec"]
+popt, pcov, R2=fit_mobile(mec)
+stampa(popt, pcov, R2, len(mec.index))
+Evst(mec, popt)
+#plot dello scostamento dell'energia nel tempo dal valore iniziale
+delta(mec)
 #"""
-
-"""
-#ene=ene.head(int(len(ene.index)/10))
+#"""fit e grafici energia cinetica e potenziale
 mec=ene['-Ecin']
-ndata = len(ene.index)
-guess=[mec.mean(), 15e33, 12.03, np.pi/2, 2.5e27	, 0.2405, 0, -0]
-inf=[min(mec), 5e33, 10, -np.pi, 1.5e27, 0.235, -np.pi, -np.inf]
-sup=[max(mec), 5e35, 15, np.pi, 10e27, 0.25, np.pi, 0]
-popt, pcov = sp.optimize.curve_fit(cos_func, ene.index, ene['Emec'], p0=guess, bounds=[inf,sup])
-sigma = np.sqrt(ndata * np.diag(pcov))
-R2 = R_squared(ene.index, mec, cos_func, *popt)
+popt, pcov, R2=fit_mobile(mec)
+stampa(popt, pcov, R2, len(mec.index))
+Evst(mec, popt)
 
-print(f"Valore di partenza dell'oscillazione = {popt[0]} +/- {sigma[1]}")
-print(f"Ampiezza portante = {popt[1]} +/- {sigma[1]}")
-print(f"Periodo portante = {popt[2]} +/- {sigma[2]}")
-print(f"fase portante = {popt[3]*180/np.pi} +/- {sigma[3]*180/np.pi}")
-print(f"Ampiezza modulante = {popt[4]} +/- {sigma[4]}")
-print(f"Periodo modulante = {popt[5]*365.26} +/- {sigma[5]*365.26}")
-print(f"fase modulante= {popt[6]*180/np.pi} +/- {sigma[6]*180/np.pi}")
-print(f"pendenza decrescita= {popt[7]} +/- {sigma[7]}")
-print(f"R^2 = {R2}")
-
-plt.figure(figsize=(18,8))
-plt.title("Energia meccanica nel tempo")
-plt.xlabel("tempo [step]")
-plt.ylabel("Energia [J]")
-plt.plot(ene.index, mec, label='Emec')
-plt.plot(ene.index, cos_func(ene.index, *popt), '-', label='Fit')
-plt.legend()
-plt.show()
-
-fig, axs = plt.subplots(2, figsize=(19,10))
-fig.suptitle('Energia meccanica nel tempo')
-axs[1].plot(ene.index, cos_func(ene.index, *popt), '-', label='Fit')
-axs[0].plot(ene.index, mec, label='Emec')
-axs[1].set_title('f(t)=A+Bcos($2 \pi t/C$-D)+Ecos($2\pi t/F$-G)+Ht')
-for ax in axs.flat:
-    ax.set(xlabel='Tempo [anni]', ylabel='Energia [J]')
-for ax in axs.flat:
-    ax.label_outer()
-plt.legend()
-plt.show()
+mec=ene['Epot/2']
+popt, pcov, R2=fit_mobile(mec)
+stampa(popt, pcov, R2, len(mec.index))
+Evst(mec, popt)
 #"""
 
+"""parametri fit su brevbe periodo a sole fisso
+popt, pcov, R2=fit_(mec)
+stampa(popt, pcov, R2, len(mec.index))
+Evst(mec, popt)
 """
-#dist=dist.head(int(len(ene.index)/10))
-d=dist['ddd']
-ndata = len(dist.index)
-guess=[d.mean(), 4.2e10, 12.03, np.pi/2, 0	, 0.2405, 0, -0]
-inf=[min(d), 1e10, 10, -np.pi, 0, 0.235, -np.pi, -np.inf]
-sup=[max(d), 5e10, 15, np.pi, 1, 0.25, np.pi, np.inf]
-popt, pcov = sp.optimize.curve_fit(cos_func, dist.index, d, p0=guess, bounds=[inf,sup])
-sigma = np.sqrt(ndata * np.diag(pcov))
-R2 = R_squared(dist.index, d, cos_func, *popt)
-
-print(f"Valore di partenza dell'oscillazione = {popt[0]} +/- {sigma[1]}")
-print(f"Ampiezza portante = {popt[1]} +/- {sigma[1]}")
-print(f"Periodo portante = {popt[2]} +/- {sigma[2]}")
-print(f"fase portante = {popt[3]*180/np.pi} +/- {sigma[3]*180/np.pi}")
-print(f"Ampiezza modulante = {popt[4]} +/- {sigma[4]}")
-print(f"Periodo modulante = {popt[5]*365.26} +/- {sigma[5]*365.26}")
-print(f"fase modulante= {popt[6]*180/np.pi} +/- {sigma[6]*180/np.pi}")
-print(f"pendenza decrescita= {popt[7]} +/- {sigma[7]}")
-print(f"R^2 = {R2}")
-
-plt.figure(figsize=(18,8))
-plt.title("Distanza di Giove dal Sole nel tempo")
-plt.xlabel("tempo [step]")
-plt.ylabel("Distanza [m]")
-plt.plot(dist.index, d, label='Emec')
-plt.plot(dist.index, cos_func(dist.index, *popt), '-', label='Fit')
-plt.legend()
-plt.show()
-
-fig, axs = plt.subplots(2, figsize=(19,10))
-fig.suptitle('Distanza Giove-SOle nel tempo')
-axs[1].plot(dist.index, cos_func(dist.index, *popt), '-', label='Fit')
-axs[0].plot(dist.index, d, label='dist')
-axs[1].set_title('f(t)=A+Bcos($2 \pi t/C$-D)+Ecos($2\pi t/F$-G)+Ht')
-for ax in axs.flat:
-    ax.set(xlabel='Tempo [anni]', ylabel='Distanza [m]')
-for ax in axs.flat:
-    ax.label_outer()
-plt.legend()
-plt.show()
-#"""
-
-#parametri fit su brevbe periodo a sole fisso
-#"""
-ene=ene.head(int(len(ene.index)/25))
-mec=ene['Emec']
-ndata = len(ene.index)
-guess=[-1.9586215e35, 0.7e28, 0.71, -np.pi, 0.7e28, 0.07, 0, -4e26]
-inf=[min(mec), 1e27, 0.235, -np.pi, 1.5e27, 0, -np.pi, -1e27]
-sup=[max(mec), 10e28, 1, np.pi, 10e28, 0.1, np.pi, -1e26]
-popt, pcov = sp.optimize.curve_fit(cos_func, ene.index, ene['Emec'], p0=guess, bounds=[inf,sup])
-sigma = np.sqrt(ndata * np.diag(pcov))
-R2 = R_squared(ene.index, mec, cos_func, *popt)
-
-print(f"Valore di partenza dell'oscillazione = {popt[0]} +/- {sigma[1]}")
-print(f"Ampiezza portante = {popt[1]} +/- {sigma[1]}")
-print(f"Periodo portante = {popt[2]} +/- {sigma[2]}")
-print(f"fase portante = {popt[3]*180/np.pi} +/- {sigma[3]*180/np.pi}")
-print(f"Ampiezza modulante = {popt[4]} +/- {sigma[4]}")
-print(f"Periodo modulante = {popt[5]*365.26} +/- {sigma[5]*365.26}")
-print(f"fase modulante= {popt[6]*180/np.pi} +/- {sigma[6]*180/np.pi}")
-print(f"pendenza decrescita= {popt[7]} +/- {sigma[7]}")
-print(f"R^2 = {R2}")
-
-plt.figure(figsize=(18,8))
-plt.title("Energia meccanica nel tempo")
-plt.xlabel("tempo [step]")
-plt.ylabel("Energia [J]")
-plt.plot(ene.index, mec, label='Emec')
-plt.plot(ene.index, cos_func(ene.index, *popt), '-', label='Fit')
-plt.legend()
-plt.show()
-
-fig, axs = plt.subplots(2, figsize=(19,10))
-fig.suptitle('Energia meccanica nel tempo')
-axs[1].plot(ene.index, cos_func(ene.index, *popt), '-', label='Fit')
-axs[0].plot(ene.index, mec, label='Emec')
-axs[1].set_title('f(t)=A+Bcos($2 \pi t/C$-D)+Ecos($2\pi t/F$-G)+Ht')
-for ax in axs.flat:
-    ax.set(xlabel='Tempo [anni]', ylabel='Energia [J]')
-for ax in axs.flat:
-    ax.label_outer()
-plt.legend()
-plt.show()
-#"""
-
-
-
-#sr = df[0][df[0]['Emec'] == max(df[0]['Emec'])]
-#print(sr)
