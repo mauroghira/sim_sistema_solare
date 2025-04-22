73bdd8f5a79bcfa91aa821514a01c9401ff94e20
Mauro Ghirardi <mauro.ghirardi01@universitadipavia.it>
2025-04-22 15:45:13 +0200

    errore

--- cppss/sistema.cpp
+++ cppss/sistema.cpp
@@ -141,7 +141,7 @@ void sistema::evodt(uint32_t mode, uint32_t rel, uint64_t j){
 	if(j%10000==0){
 		vettore dS=m_corpi[0]->P();
 		std::ofstream out;
-		out.open("ris2/dist_sole_ris2/"+m_inc, std::ofstream::app);
+		out.open("ris2/dist_sole_"+m_inc, std::ofstream::app);
 		out<<std::setprecision(10)<<float(j)*m_dT/(365*24*3600);
 		for(auto c: m_corpi){
 			vettore dd=c->P()-dS;
@@ -154,7 +154,7 @@ void sistema::evodt(uint32_t mode, uint32_t rel, uint64_t j){
 	
 	if(j%10000==0){
 		std::ofstream out;
-		out.open("ris2/incl_ris2/"+m_inc, std::ofstream::app);
+		out.open("ris2/incl_"+m_inc, std::ofstream::app);
 		out<<float(j)*m_dT/(365*24*3600);
 		for(auto c: m_corpi){
 			out<<" "<<c->incl();
@@ -167,7 +167,7 @@ void sistema::evodt(uint32_t mode, uint32_t rel, uint64_t j){
 	if(float(j)*m_dT/(365*24*3600) >= 4999){
 	vettore dS=m_corpi[0]->P();
 	std::ofstream out;
-	out.open("ris2/val_err_ris2/"+m_inc, std::ofstream::app);
+	out.open("ris2/val_err_"+m_inc, std::ofstream::app);
 	for(auto c: m_corpi){
 		vettore dd=c->P()-dS;
 		float d=(float)dd.modulo();
@@ -302,7 +302,7 @@ void sistema::output(std::string file){
 }
 
 void sistema::savehist(std::string out){
-	std::string fx="ris2/histo_ris2/"+out;
+	std::string fx="ris2/histo_"+out;
 	TFile* f(TFile::Open(fx.c_str(), "recreate"));
 	for(auto P: m_corpi){
 		for(int i=0; i<P->numHistos(); i++){
@@ -352,11 +352,11 @@ void sistema::mkGraf(std::string pla){
 }
 
 void sistema::clean(){	
-	std::ofstream out("ris2/dist_sole_ris2/"+m_inc);
+	std::ofstream out("ris2/dist_sole_"+m_inc);
 	out.close();
-	out.open("ris2/incl_ris2/"+m_inc);
+	out.open("ris2/incl_"+m_inc);
 	out.close();
-	out.open("ris2/val_err_ris2/"+m_inc);
+	out.open("ris2/val_err_"+m_inc);
 	for(auto p: m_corpi) out<<p->NAME()<<" ";
 	out<<std::endl;
 	out.close();	
